766b24fa1ef86d076b768c72ccdc65fe0e202218
SessionHu <102411014+SessionHu@users.noreply.github.com>
2024-07-02 15:31:11 +0800

    feat: 农历 & 更好的无JS支持

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,9 @@
 
 // redirect
 {
-    if (window.location.pathname !== "/") {
+    if (window.location.pathname === "/index.html") {
+        window.location.pathname = "/";
+    } else if (window.location.pathname !== "/") {
         window.location.replace(
             window.location.protocol + "//" + window.location.host + "/#" +
             window.location.pathname.replace(".html", "") + "/" +
@@ -58,143 +60,199 @@
 
 }
 
-// main
-{
+class Sess {
 
     /**
-     * Add .layui-this.
+     * Open a dialog about the Error.
+     * @param {Error} e 
      */
-    const navthis = () => {
-        const path = layui.url().hash.path;
-        if (path.length < 1 || path[0] === "") {
-            document.getElementById("nav-index").classList.add("layui-this");
-            return;
-        }
-        switch (path[0]) {
-            case "home":
-                document.getElementById("nav-index").classList.add("layui-this");
-                break;
-            case "category":
-                document.getElementById("nav-category").classList.add("layui-this");
-                break;
-            case "about":
-                document.getElementById("nav-about").classList.add("layui-this");
-                break;
-            default:
-            // nothing here...
-        }
+    static openErrLayer(e) {
+        layer.open({
+            type: 0,
+            title: e.name,
+            content: e.stack.replaceAll(" at", "<br />&nbsp;&nbsp;&nbsp;&nbsp;at"),
+            icon: 2,
+            skin: "layui-layer-win10",
+            shade: .01,
+            shadeClose: true
+        })
     }
-    navthis();
 
     /**
-     * Load Socialist Core Values.
+     * Render carousel.
      */
-    const sccrval = () => {
-        const scv = [
-            "富强", "民主", "文明", "和谐",
-            "自由", "平等", "公正", "法治",
-            "爱国", "敬业", "诚信", "友善"
-            // "功德 +1"
-        ];
-        const colors = [
-            "layui-font-red", "layui-font-orange", "layui-font-green", "layui-font-cyan",
-            "layui-font-blue", "layui-font-purple", "layui-font-black", "layui-font-gray"
-        ];
-        let scvi = 0;
-        let colori = 0;
-        window.addEventListener("click", (ev) => {
-            const scvt = this.document.createElement("span");
-            // basic style
-            scvt.style.position = "fixed";
-            scvt.style.top = `${ev.y - 16}px`;
-            scvt.style.left = `${ev.x}px`;
-            scvt.style.fontWeight = "bold";
-            scvt.style.whiteSpace = "nowrap";
-            scvt.style.userSelect = "none";
-            scvt.style.opacity = "1";
-            scvt.style.zIndex = "1145141919";
-            // layui style
-            scvt.className = colors[colori++];
-            scvt.innerText = scv[scvi++];
-            if (colori >= colors.length) colori = 0;
-            if (scvi >= scv.length) scvi = 0;
-            // show
-            this.document.body.appendChild(scvt);
-            // remove
-            let k = 1;
-            const itv = window.setInterval(() => {
-                scvt.style.opacity = parseFloat(scvt.style.opacity) - .02;
-                scvt.style.top = `${parseFloat(scvt.style.top.substring(0, scvt.style.top.length - 2)) - (k += .1)}px`;
-                if (scvt.style.opacity <= 0) {
-                    scvt.remove();
-                    window.clearInterval(itv);
-                }
-            }, 20);
-        });
-    }
-    sccrval();
-
-    /**
-     * Render carousel
-     */
-    const renderCarousel = () => {
+    static renderCarousel() {
         if (document.querySelector(".layui-carousel > div[carousel-item] > *")) {
             layui.carousel.render({
                 elem: ".layui-carousel",
-                width: "auto",
-                interval: 5e3
+                width: "auto"
             });
         }
     }
 
     /**
-     * Render breadcrumb
+     * Render breadcrumb.
      */
-    const renderBreadcrumb = () => {
+    static renderBreadcrumb() {
         layui.element.render('breadcrumb', "bc");
     }
 
     /**
-     * Load #main content.
-     * @param {JQuery} elem
+     * Main.
      */
-    const loadMainContent = async (elem = { context: { parentElement: { id: null } } }) => {
-        // param
-        let path = layui.url().hash.path;
-        if (elem.context.parentElement.id !== null) path = [null];
-        // get HTML in #main
-        const maincontainer = document.createElement("div");
-        if (elem.context.parentElement.id === "nav-index" || path.length < 1 || path[0] === "" || path[0] === "home") {
-            maincontainer.innerHTML = await (await fetch("/home.html")).text();
-        } else {
-            maincontainer.innerHTML = '<div id="main">404</div>';
-        }
-        document.getElementById("main-title").innerHTML = maincontainer.querySelector("#main-title").innerHTML;
-        document.getElementById("main").innerHTML = maincontainer.querySelector("#main").innerHTML;
-        // render
-        renderBreadcrumb();
-        renderCarousel();
-    }
-    loadMainContent();
-    layui.element.on("nav(nav-sess)", (elem) => loadMainContent(elem));
+    static async main() {
 
-    /**
-     * @param {HTMLElement} this 
-     */
-    const showCarouselPhotos = function () {
-        layer.photos({
-            photos: `div[lay-on=${this.getAttribute("lay-on")}]`
+        /**
+         * Add .layui-this.
+         */
+        const navthis = () => {
+            const path = layui.url().hash.path;
+            if (path.length < 1 || path[0] === "") {
+                document.getElementById("nav-index").classList.add("layui-this");
+                return;
+            }
+            switch (path[0]) {
+                case "home":
+                    document.getElementById("nav-index").classList.add("layui-this");
+                    break;
+                case "category":
+                    document.getElementById("nav-category").classList.add("layui-this");
+                    break;
+                case "about":
+                    document.getElementById("nav-about").classList.add("layui-this");
+                    break;
+                default:
+                // nothing here...
+            }
+        };
+        navthis();
+
+        /**
+         * Load Socialist Core Values.
+         */
+        const sccrval = () => {
+            const scv = [
+                "富强", "民主", "文明", "和谐",
+                "自由", "平等", "公正", "法治",
+                "爱国", "敬业", "诚信", "友善"
+                // "功德 +1"
+            ];
+            const colors = [
+                "layui-font-red", "layui-font-orange", "layui-font-green", "layui-font-cyan",
+                "layui-font-blue", "layui-font-purple", "layui-font-black", "layui-font-gray"
+            ];
+            let scvi = 0;
+            let colori = 0;
+            window.addEventListener("click", (ev) => {
+                const scvt = document.createElement("span");
+                // basic style
+                scvt.style.position = "fixed";
+                scvt.style.top = `${ev.y - 16}px`;
+                scvt.style.left = `${ev.x}px`;
+                scvt.style.fontWeight = "bold";
+                scvt.style.whiteSpace = "nowrap";
+                scvt.style.userSelect = "none";
+                scvt.style.opacity = "1";
+                scvt.style.zIndex = "1145141919";
+                // layui style
+                scvt.className = colors[colori++];
+                scvt.innerText = scv[scvi++];
+                if (colori >= colors.length) colori = 0;
+                if (scvi >= scv.length) scvi = 0;
+                // show
+                document.body.appendChild(scvt);
+                // remove
+                let k = 1;
+                const itv = window.setInterval(() => {
+                    scvt.style.opacity = parseFloat(scvt.style.opacity) - .02;
+                    scvt.style.top = `${parseFloat(scvt.style.top.substring(0, scvt.style.top.length - 2)) - (k += .1)}px`;
+                    if (scvt.style.opacity <= 0) {
+                        scvt.remove();
+                        window.clearInterval(itv);
+                    }
+                }, 20);
+            });
+        };
+        sccrval();
+
+        /**
+         * Load #main content.
+         * @param {JQuery} elem
+         */
+        const loadMainContent = async (elem = { context: { parentElement: { id: null } } }) => {
+            // param
+            let path = layui.url().hash.path;
+            if (elem.context.parentElement.id !== null) path = [null];
+            // request path
+            let reqpath = "";
+            if (elem.context.parentElement.id === "nav-index" || path.length < 1 || path[0] === "" || path[0] === "home") {
+                reqpath = "/home.html";
+            } else if (elem.context.parentElement.id === "nav-category" || path[0] === "category") {
+                reqpath = "/category.html";
+            } else if (elem.context.parentElement.id === "nav-about" || path[0] === "about") {
+                reqpath = "/about.html";
+            } else {
+                for (const t of path) {
+                    reqpath += "/";
+                    reqpath += t;
+                }
+                reqpath += ".html";
+            }
+            // get HTML in #main
+            const maincontainer = document.createElement("div");
+            try {
+                const response = await fetch(reqpath);
+                if (response.ok) {
+                    maincontainer.innerHTML = await response.text();
+                } else {
+                    throw new Error(`${response.status} ${response.statusText}`);
+                }
+            } catch (e) {
+                Sess.openErrLayer(e);
+            }
+            if (maincontainer.innerHTML === "") maincontainer.innerHTML = `
+                <h1 id="main-title">404 Not Found</h1>
+                <div id="main">There is nothing you wanted...</div>
+            `;
+            document.getElementById("main-title").innerHTML = maincontainer.querySelector("#main-title").innerHTML;
+            document.getElementById("main").innerHTML = maincontainer.querySelector("#main").innerHTML;
+            // render
+            this.renderBreadcrumb();
+            this.renderCarousel();
+        };
+        await loadMainContent();
+        layui.element.on("nav(nav-sess)", (elem) => loadMainContent(elem));
+
+        /**
+         * @param {HTMLElement} this
+         */
+        const showCarouselPhotos = function () {
+            layer.photos({
+                photos: `div[lay-on=${this.getAttribute("lay-on")}]`
+            });
+        };
+        layui.util.on("lay-on", {
+            "carousel-profile-3d-contrib": showCarouselPhotos,
+            "carousel-bing": showCarouselPhotos,
+            "carousel-outer-space": showCarouselPhotos,
+            "carousel-720x360-5": showCarouselPhotos
         });
-    };
-    layui.util.on("lay-on", {
-        "carousel-profile-3d-contrib": showCarouselPhotos,
-        "carousel-outer-space": showCarouselPhotos,
-        "carousel-720x360-5": showCarouselPhotos
-    });
 
-    // debug
-    // window.addEventListener("resize", () => {
-    //     layer.msg("w:" + window.innerWidth + "<br>h:" + window.innerHeight);
-    // });
+        await this.fortune();
+
+        // debug
+        // window.addEventListener("resize", () => {
+        //     layer.msg("w:" + window.innerWidth + "<br>h:" + window.innerHeight);
+        // });
+
+    }
+
+    static async fortune() {
+        const text = await (await fetch("https://v1.hitokoto.cn/?encode=text")).text();
+        const elem = document.querySelector("#lunar-calendar-container > .layui-card-body > p"); 
+        elem.innerText = text;
+    }
 
 }
+
+Sess.main().catch((e) => Sess.openErrLayer(e));
---
+++ scripts/laylunar.js
@@ -0,0 +1,121 @@
+{
+    const fun = () => {
+        try {
+            const container = document.querySelector("div#lunar-calendar-container");
+            if (362 > container.getBoundingClientRect().width) {
+                container.querySelector(".layui-card-body > div").style.display = "none";
+                container.querySelector(".layui-card-body > p").style.display = "inline";
+                container.querySelector(".layui-card-header").innerText = "Fortune";
+            } else {
+                container.querySelector(".layui-card-body > div").style.display = "block";
+                container.querySelector(".layui-card-body > p").style.display = "block";
+                container.querySelector(".layui-card-header").innerText = "农历";
+            }
+        } catch (e) {
+            Sess.openErrLayer(e);
+        }
+    }
+    window.addEventListener("resize", fun);
+    window.addEventListener("load", fun);
+}
+
+// https://layui.dev/docs/2/laydate/#demo-custom-cell
+layui.use(function () {
+    const $ = layui.$;
+    // 渲染
+    layui.laydate.render({
+        elem: '#lunar-calendar-container > .layui-card-body > div',
+        position: 'static',
+        // value: '2024-03-30',
+        isPreview: false,
+        btns: ['now'],
+        theme: 'lunar',
+        autoConfirm: false,
+        ready: function (date) {
+            if (!this._previewEl) {
+                const key = this.elem.attr('lay-key');
+                const panelEl = $('#layui-laydate' + key);
+                this._previewEl = panelEl.find('.layui-laydate-preview');
+                this.cellRender(date);
+            }
+        },
+        change: function (value, date) {
+            this.cellRender(date);
+        },
+        onNow: function (value, date) {
+            this.cellRender(date);
+        },
+        cellRender: function (ymd, render, info) {
+            const y = ymd.year;
+            const m = ymd.month;
+            const d = ymd.date;
+            const lunarDate = Solar.fromYmd(y, m, d).getLunar();
+            const lunar = lunarDate.getDayInChinese();
+            const jieQi = lunarDate.getJieQi();
+            const holiday = HolidayUtil.getHoliday(y, m, d);
+            const displayHoliday = holiday && holiday.getTarget() === holiday.getDay() ? holiday.getName() : undefined;
+            const displayHolidayBadge = holiday && holiday.getTarget() ? (holiday.isWork() ? '班' : '休') : undefined;
+            const isHoliday = holiday && holiday.getTarget() && !holiday.isWork();
+            // 在预览区显示自定义农历相关信息
+            if (this._previewEl && (!info || (info && info.type === "date"))) {
+                const holidayBadgeStyle = [
+                    'color:#fff',
+                    'background-color:' + (isHoliday ? '#eb3333' : '#333'),
+                    'display:' + (displayHolidayBadge ? 'inline-block' : 'none')
+                ].join(';')
+                const festivalBadgeStyle = [
+                    'color:#fff',
+                    'background-color:#1e9fff',
+                    'display:' + (displayHoliday || jieQi ? 'inline-block' : 'none')
+                ].join(';')
+                const tipsText = [
+                    '<div class="preview-inner">',
+                    '<div style="/*color:#333;*/">农历' + lunarDate.getMonthInChinese() + '月' + lunarDate.getDayInChinese() + '</div>',
+                    '<div style="font-size:10px">' + lunarDate.getYearInGanZhi() + lunarDate.getYearShengXiao() + '年</div>',
+                    '<div style="font-size:10px">' + lunarDate.getMonthInGanZhi() + '月 ' + lunarDate.getDayInGanZhi() + '日</div>',
+                    '<div class="badge" style="' + holidayBadgeStyle + '">' + displayHolidayBadge + '</div>',
+                    '<div class="badge" style="' + festivalBadgeStyle + '">' + (displayHoliday || jieQi) + '</div>',
+                    '</div>'
+                ].join('');
+                this._previewEl.html(tipsText);
+            };
+            if (!render) return;
+            // 面板类型
+            if (info.type === 'date') {
+                const clazz = [
+                    'date-cell-inner',
+                    isHoliday ? 'holiday' : '',
+                    displayHoliday || jieQi ? 'hightlight' : '',
+                ].join(' ');
+                const content = [
+                    '<div class="' + clazz + '">',
+                    '<b>' + d + '</b>',
+                    '<i>' + (displayHoliday || jieQi || lunar) + '</i>',
+                    displayHolidayBadge ? '<u class="badge">' + displayHolidayBadge + '</u>' : '',
+                    '</div>',
+                ].join('');
+                // render(content)
+                // render($(content)[0])
+                const contentEl = $(content);
+                contentEl.on('contextmenu', function (e) {
+                    e.preventDefault();
+                    layer.tips(lunarDate.toString(), this, {
+                        tips: [1, '#16baaa'],
+                        zIndex: 999999999,
+                    });
+                });
+                render(contentEl);
+            } else if (info.type === 'year') {
+                const lunarDate = Lunar.fromDate(new Date(y + 1, 0));
+                const lunar = lunarDate.getYearInGanZhi() + lunarDate.getYearShengXiao();
+                render([
+                    y + '年',
+                    '<div style="font-size:12px">' + lunar + '年</div>',
+                ].join(''));
+            } else if (info.type === 'month') {
+                const lunar = lunarDate.getMonthInChinese();
+                render([m + '月(' + lunar + '月)'].join(''));
+            }
+        },
+    });
+});
