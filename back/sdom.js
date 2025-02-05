61675795bd2b281bdd39c9907f4ae6321ca12371
SessionHu <102411014+SessionHu@users.noreply.github.com>
2025-02-05 12:57:12 +0800

    feat(sdom.js): simple DOM-like processor

---
+++ back/sdom.js
@@ -0,0 +1,205 @@
+export class Node {
+
+  get textContent() {
+    let r = '';
+    for (const e of this.#childNodes) {
+      r += e.textContent;
+    }
+    return r;
+  }
+
+  /** @param {string} textContent */
+  set textContent(textContent) {
+    this.#childNodes = new Array();
+    this.appendChild(new TextNode(textContent));
+  }
+
+  /** @type {Node[]} */
+  #childNodes = new Array();
+  get childNodes() {
+    return this.#childNodes;
+  }
+
+  /** @type {Node | null} */
+  #parentNode = null;
+  get parentNode() {
+    return this.#parentNode;
+  }
+
+  /**
+   * @param {Node} node
+   */
+  appendChild(node) {
+    if (node instanceof Document) return;
+    node.#parentNode?.removeChild(node);
+    node.#parentNode = this;
+    this.#childNodes.push(node);
+  }
+
+  /**
+   * @param {Node} node
+   */
+  removeChild(node) {
+    const i = this.#childNodes.indexOf(node);
+    this.#childNodes.splice(i, 1).forEach(e => e.#parentNode = null);
+  }
+
+  /**
+   * @returns {string}
+   */
+  toXML() {
+    return this.#childNodes.map((n) => n.toXML()).join('');
+  }
+
+}
+
+export class TextNode extends Node {
+
+  /**
+   * @param {string?} textContent
+   */
+  constructor(textContent) {
+    super();
+    if (textContent) this.textContent = textContent;
+  }
+
+  /** @type {string} */
+  #textContent = '';
+  get textContent() {
+    return this.#textContent;
+  }
+  /** @param {string} textContent */
+  set textContent(textContent) {
+    this.#textContent = typeof textContent === 'string' ? textContent : String(textContent);
+  }
+
+  toXML() {
+    return encodeXML(this.#textContent);
+  }
+
+}
+
+export class Element extends Node {
+
+  /**
+   * @param {string} tagName
+   */
+  constructor(tagName) {
+    super();
+    this.#tagName = tagName;
+  }
+
+  /**
+   * @param {string} tagName
+   */
+  static new(tagName) {
+    return new Element(tagName);
+  }
+
+  #tagName;
+  get tagName() {
+    return this.#tagName;
+  }
+
+  /** @type {Attr[]} */
+  #attributes = new Array();
+  get attributes() {
+    return this.#attributes;
+  }
+
+  /**
+   * @param {string} name
+   */
+  getAttribute(name) {
+    return this.#attributes.find((a) => a.name === name)?.value;
+  }
+
+  /**
+   * @param {string} name
+   * @param {string} value
+   */
+  setAttribute(name, value) {
+    for (const a of this.#attributes) {
+      if (a.name === name) return void (a.value = value);
+    }
+    this.#attributes.push(new Attr(name, value));
+  }
+
+  toXML() {
+    const attrstr = this.#attributes.map((a) => a.toXML()).join(' ');
+    if (!this.childNodes.length) {
+      return `<${encodeXML(this.tagName)} ${this.#attributes.length ? attrstr + ' ' : ''}/>`;
+    } else {
+      return `<${encodeXML(this.tagName)} ${attrstr}>${
+        this.childNodes.map((e) => e.toXML()).join('')
+      }</${encodeXML(this.#tagName)}>`;
+    }
+  }
+
+}
+
+class Attr {
+
+  #name;
+  get name() {
+    return this.#name;
+  }
+
+  /**
+   * @param {string} name
+   * @param {string} value
+   */
+  constructor(name, value) {
+    this.#name = name;
+    this.value = value;
+  }
+
+  toXML() {
+    return `${encodeXML(this.#name)}="${encodeXML(this.value)}"`;
+  }
+
+}
+
+export class Document extends Node {
+
+  constructor() {
+    super();
+  }
+
+  /**
+   * @param {Node} node
+   */
+  appendChild(node) {
+    this.childNodes.splice(0, this.childNodes.length, node);
+  }
+
+  toXML() {
+    return '<?xml version="1.0" encoding="UTF-8"?>' + this.childNodes.map((e) => e.toXML()).join('');
+  }
+
+  /**
+   * @param {string} tagName
+   */
+  createElement(tagName) {
+    return new Element(tagName);
+  }
+
+  /**
+   * @param {string} text
+   */
+  createTextNode(text) {
+    return new TextNode(text);
+  }
+
+}
+
+/**
+ * @param {string} text
+ */
+export function encodeXML(text) {
+  return text.replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll("'", '&apos;')
+    .replaceAll('"', '&quot;');
+}
