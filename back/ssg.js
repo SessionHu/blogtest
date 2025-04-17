04b7a115ff719d242756ac985829fe6e20fea4d8
SessionHu <102411014+SessionHu@users.noreply.github.com>
2025-04-17 13:45:48 +0800

    feat: ssg for page title

--- back/ssg.js
+++ back/ssg.js
@@ -19,9 +19,9 @@ export async function render(fname, cache = {}) {
   const extn = path.extname(fname = path.normalize(fname)).toLowerCase();
   if (extn === ".html") {
     if (fname === 'front/home.html') return renderHomeHTML(cache);
-    if (fname === 'front/category.html') return renderCategoryHTML(cache);
-    if (fname === 'front/friends.html') return renderFriendHTML(cache);
-    return renderHTML(readHTML(fname).then((res) => res.toString()), cache);
+    else if (fname === 'front/category.html') return renderCategoryHTML(cache);
+    else if (fname === 'front/friends.html') return renderFriendHTML(cache);
+    else return renderHTML(readHTML(fname).then((res) => res.toString()), cache, fname === 'front/about.html' ? '关于 - SESSのB10GTEST' : void 0);
   } else if (extn === ".md") {
     return renderMarkdown(fname, cache);
   } else {
@@ -48,15 +48,21 @@ async function readHTML(fname, options = void 0) {
 }
 
 /**
- * @param {SSGCache} cache
+ * @param {SSGCache} [cache]
+ * @param {string} [title]
  * @returns {Promise<string>}
  */
-async function readBaseHTML(cache = {}) {
+async function readBaseHTML(cache = {}, title = 'SESSのB10GTEST') {
   /** @type {FriendsJson} */
   const fj = cache.friends || (cache.friends = JSON.parse(await fs.readFile('front/friends.json', 'utf8')));
   shufArray(fj.friends);
   shufArray(fj.organizations);
-  return cache.basehtml || (cache.basehtml = (await readHTML('./front/index.html')).toString().replace('{{FRIENDS-JSON}}', '<noscript>' + encodeXML(JSON.stringify(fj)) + '</noscript>').replace('{{POSTS-COUNT}}', ((await getPostsCount(cache)).toString())));
+  if (!cache.basehtml)
+    cache.basehtml = (await readHTML('./front/index.html')).toString();
+  return cache.basehtml
+    .replace('{{FRIENDS-JSON}}', '<noscript>' + encodeXML(JSON.stringify(fj)) + '</noscript>')
+    .replace('{{POSTS-COUNT}}', ((await getPostsCount(cache)).toString()))
+    .replace('{{PAGE-TITLE}}', title);
 }
 
 /**
@@ -89,10 +95,12 @@ async function getPostsCount(cache = {}) {
 
 /**
  * @param {string | Promise<string>} content
- * @param {SSGCache} cache
+ * @param {SSGCache} [cache]
+ * @param {string} [title]
  */
-async function renderHTML(content, cache = {}) {
-  return (await readBaseHTML(cache)).replace('{{MAIN-CONTENT}}', (await content).toString());
+async function renderHTML(content, cache = {}, title) {
+  return (await readBaseHTML(cache, title))
+    .replace('{{MAIN-CONTENT}}', (await content).toString());
 }
 
 /**
@@ -145,7 +153,7 @@ async function renderMarkdown(fname, cache = {}) {
   }
   // return
   if (!date) date = new Date(0);
-  return (await readBaseHTML(cache)).replace('{{MAIN-CONTENT}}', `
+  return (await readBaseHTML(cache, titleName + ' - SESSのB10GTEST')).replace('{{MAIN-CONTENT}}', `
     <div class="layui-panel layui-card radius">
       <h1 id="main-title" class="layui-card-header">
         <span class="layui-breadcrumb ws-nowrap" lay-separator=">">
@@ -243,7 +251,13 @@ async function renderCategoryHTML(cache = {}) {
     elem.childNodes[1].appendChild(anchor);
     elem.childNodes[1].appendChild(Element.new('br'));
   }
-  return renderHTML((await catehtml).toString().replace('{{CATEGORY-CONTENT}}', colladiv.toXML()), cache);
+  return renderHTML(
+    (await catehtml)
+      .toString()
+      .replace('{{CATEGORY-CONTENT}}', colladiv.toXML()),
+    cache,
+    '分类 - SESSのB10GTEST'
+  );
 }
 
 /**
@@ -294,7 +308,14 @@ async function renderFriendHTML(cache = {}) {
   };
   const frs = fj.friends.map((f) => genFriendLinkElem(f, false).toXML()).join('');
   const ogs = fj.organizations.map((f) => genFriendLinkElem(f, true).toXML()).join('');
-  return (await readBaseHTML(cache)).replace('{{MAIN-CONTENT}}', (await frndhtml).toString().replace('{{FRIENDS-REAL}}', frs).replace('{{FRIENDS-ORGS}}', ogs));
+  return (await readBaseHTML(cache, '友链 - SESSのB10GTEST'))
+    .replace(
+      '{{MAIN-CONTENT}}',
+      (await frndhtml)
+        .toString()
+        .replace('{{FRIENDS-REAL}}', frs)
+        .replace('{{FRIENDS-ORGS}}', ogs)
+    );
 }
 
 /**
